a5269b4abb090250720645ece8cb1d220129f4c6
JoaoCaua <sortinho100@outlook.com>
2025-02-27 22:28:25 -0300

    Teste

--- src/main/java/com/fatec/projeto/projeto2025/controllers/ExercicioController.java
+++ src/main/java/com/fatec/projeto/projeto2025/controllers/ExercicioController.java
@@ -41,4 +41,62 @@ public class ExercicioController {
             return "idade invalida";
         }
     }
+
+    @GetMapping("/get-numero/{numero}")
+    public String RetornaNumero(@PathVariable Integer numero){
+        try{
+            if(numero < 0){
+                throw new NumberFormatException();
+            }
+
+            else if(numero % 2 == 0){
+                return "O número " + numero + " é par";
+            }
+            else{
+                return "O número " + numero + " é ímpar";
+            }
+        }
+
+        catch(NumberFormatException e){
+            return "Número inválido";
+        }
+    }
+
+    @GetMapping("/get-imc/{peso}/{altura}")
+    public String RetornaImc(@PathVariable Double peso, @PathVariable Double altura){
+        try{
+            Double imc = peso / (altura*altura);
+            if(imc < 0){
+                throw new NumberFormatException();
+            }
+
+            else if(imc < 18.5){
+                return "Abaixo do peso";
+            }
+
+            else if(imc >= 18.5 && imc <= 24.9){
+                return "Peso normal";
+            }
+
+            else if(imc >= 25.0 && imc <= 29.9){
+                return "Sobrepeso";
+            }
+
+            else if(imc >= 30.0 && imc <= 34.9){
+                return "Obesidade grau 1";
+            }
+
+            else if(imc >= 35.0 && imc <= 39.9){
+                return "Obesidade grau 2";
+            }
+
+            else{
+                return "Obesidade grau 3";
+            }
+        }
+
+        catch(NumberFormatException e){
+            return "Dados inválidos";
+        }
+    }
 }
