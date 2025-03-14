c31f9093c44075a9c67dfb3d4ed5e696a73c7ab1
JoaoCaua <sortinho100@outlook.com>
2025-03-13 21:17:18 -0300

    teste

---
+++ src/main/java/com/fatec/projeto/projeto2025/controllers/Cliente.java
@@ -0,0 +1,35 @@
+package com.fatec.projeto.projeto2025.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class Cliente{
+    private Long id;
+    private String nome;
+}
+
+@RestController
+@RequestMapping("/api/cliente")
+public class ClienteController{
+    private static final Logger logger = LoggerFactory.getLogger(ClienteController.class.getName())
+;
+    private final List<Cliente> clientes = new ArrayList<>();
+    private Long idCount = 1L;
+
+    http://localhost:8080/api/cliente/criarCliente => POST
+    @PostMapping("/criarCliente")
+    public String CriarCliente(@RequestBody Cliente cliente){
+        cliente.setId(idCount++);
+        clientes.add(cliente);
+
+        logger.info(format:"Recebido JSON: Nome={}, Idade={}", cliente.getNome(), cliente.getIdade());
+        return "O cliente "+cliente.getNome()+" de idade"+cliente.getIdade()+" foi criado";
+    }
+
+    @GetMapping("path")
+    public List<Cliente> ListarClientes(){
+        return clientes;
+    }
+}
