ea8a117d297583ceb854a61cce1c58f94bb6146a
JoaoCaua <sortinho100@outlook.com>
2025-03-19 19:43:07 -0300

    SwaggerConf2

--- src/main/java/com/fatec/projeto/projeto2025/controllers/ClienteController.java
+++ src/main/java/com/fatec/projeto/projeto2025/controllers/ClienteController.java
@@ -15,7 +15,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import java.util.ArrayList;
 import java.util.List;
-// import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/api/cliente")
